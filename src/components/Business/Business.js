7e46acfe9cca8896fe5d9e2bc4d5d3f6745095f5
Project <oladosu777@gmail.com>
2018-11-28 11:13:47 +0100

    Major update

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Business.css';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -18,23 +19,25 @@ class Business extends React.Component{
 		return (
 		<div className="Business">
 		  <div className="image-container">
-		    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+		    {business.imageSrc}
 		  </div>
-		  <h2>MarginOtto Pizzeria</h2>
+		  <h2>{business.name}</h2>
 		  <div className="Business-information">
 		    <div className="Business-address">
-		      <p>1010 Paddington Way</p>
+		      <p>{business.address}</p>
 		      <p>Bordertown</p>
 		      <p>NY 10101</p>
 		    </div>
 		    <div className="Business-reviews">
-		      <h3>ITALIAN</h3>
-		      <h3 className="rating">4.5 stars}</h3>
-		      <p>90 reviews</p>
+		      <h3>{business.category}</h3>
+		      <h3 className="rating">{business.rating} stars</h3>
+		      <p>{business.reviewCount} reviews</p>
 		    </div>
 		  </div>
 		</div>
 
 		)
 	}
-}
+};
+
+export default Business;
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component{
+	render(){
+		return(
+			<div className="BusinessList">
+			  <Business /> 
+			  <Business /> 
+			  <Business /> 
+			  <Business /> 
+			  <Business /> 
+			  <Business /> 
+			</div>
+
+		)
+	}
+}
