0d4ce3b3408b939175fdc2e9e9363c264be0a4bc
Project <oladosu777@gmail.com>
2018-11-29 12:34:32 +0100

    mir

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,7 +9,7 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component{
-			/*renderSortByOptions() {
+			renderSortByOptions() {
   				return Object.keys(sortByOptions).map(sortByOption => {
   				let sortByOptionValue = sortByOptions[sortByOption];
   				return<li key={sortByOptionValue}>{sortByOption}</li>
