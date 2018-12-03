d8f5f1d8b127ba1f2ea6576adfe6f2086a72f716
Project <oladosu777@gmail.com>
2018-12-03 10:41:55 +0100

    little1

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -9,19 +9,19 @@ class Business extends React.Component{
 		return (
 		<div className="Business">
 		  <div className="image-container">
-		    {business.imageSrc}
+		    {this.props.business.imageSrc}
 		  </div>
-		  <h2>{business.name}</h2>
+		  <h2>{this.props.business.name}</h2>
 		  <div className="Business-information">
 		    <div className="Business-address">
-		      <p>{business.address}</p>
+		      <p>{this.props.business.address}</p>
 		      <p>Bordertown</p>
 		      <p>NY 10101</p>
 		    </div>
 		    <div className="Business-reviews">
-		      <h3>{business.category}</h3>
-		      <h3 className="rating">{business.rating} stars</h3>
-		      <p>{business.reviewCount} reviews</p>
+		      <h3>{this.props.business.category}</h3>
+		      <h3 className="rating">{this.props.business.rating} stars</h3>
+		      <p>{this.props.business.reviewCount} reviews</p>
 		    </div>
 		  </div>
 		</div>
