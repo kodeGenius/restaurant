4401e52d98523d56b86f86db0c81c783877494a5
Project <oladosu777@gmail.com>
2018-12-05 12:18:58 +0100

    todays latest commi

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,5 +3,27 @@ const apikey = "";
 const Yelp = {};
 
 search(term, location, sortBy){
-	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, )
+	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+  headers: {
+    Authorization: `Bearer ${apiKey}` 
+   } 
+ }).then(response =>{return response.json();
+ }).then(jsonResponse => { if(jsonResponse.businesses){
+ 	return jsonResponse.businesses.map(business => {
+ 		return {
+ 			id: business.id,
+  			imageSrc: business.image_url,
+  			name: business.name,
+			address: business.address,	
+			city: business.city,
+			state: business.state,
+			zipCode: business.zipCode,
+			category: business.category,
+			rating: business.rating
+			reviewCount: business.reviewCount
+ 		}
+ 	});
+ }
+
+ });
 }
