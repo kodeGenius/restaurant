7d47a791e47190769dd0a6f3a698f32a35e53f7c
Project <oladosu777@gmail.com>
2018-12-08 14:47:49 +0100

    little troubleshooti

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,31 +1,43 @@
 const apikey = "YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBPa7BVf9gQjKGKXmbYyNpPoMON5kbcTFPJmH7zpdlvGHdD45_q3wV6obUYv4IXHYx";
 
-const Yelp = {};
-
-search(term, location, sortBy){
-	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
-  headers: {
-    Authorization: `Bearer ${apiKey}` 
-   } 
- }).then(response =>{return response.json();
- }).then(jsonResponse => { if(jsonResponse.businesses){
- 	return jsonResponse.businesses.map(business => {
- 		return {
- 			id: business.id,
-  			imageSrc: business.image_url,
-  			name: business.name,
-			address: business.address,	
-			city: business.city,
-			state: business.state,
-			zipCode: business.zipCode,
-			category: business.category,
-			rating: business.rating
-			reviewCount: business.reviewCount
- 		}
- 	});
- }
-
- });
-}
+const Yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.address,
+              city: business.city,
+              state: business.state,
+              zipCode: business.zipCode,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.reviewCount
+            };
+          });
+        }
+      });
+  }
+};
 
 export default Yelp;
+
+
+
+
+
+
