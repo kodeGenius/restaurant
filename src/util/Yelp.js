734e1e579436cb59ac4eb99d61b4be596068465b
Project <oladosu777@gmail.com>
2018-12-08 15:43:41 +0100

    little tro

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -20,10 +20,10 @@ const Yelp = {
               id: business.id,
               imageSrc: business.image_url,
               name: business.name,
-              address: business.address,
+              address: business.location.address1,
               city: business.location.city,
               state: business.location.state,
-              zipCode: business.zipCode,
+              zipCode: business.location.zipCode,
               category: business.categories[0].title,
               rating: business.rating,
               reviewCount: business.reviewCount
