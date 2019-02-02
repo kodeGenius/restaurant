d5e9f460815ebbd0b35e6b551db31d60af7f1acc
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2019-02-02 20:06:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Yelp from './util/Yelp';
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Flavourtown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
