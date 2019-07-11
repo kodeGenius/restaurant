e83ff37f62915d3d729e56f67f2989035d18e0b1
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2019-07-11 21:25:53 +0100

    use your own key

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,4 @@
-const apikey = "YqRScCgClJAqWZWGIJCNrJWkN0Y6nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBPa7BVf9gQjKGKXmbYyNpPoMON5kbcTFPJmH7zpdlvGHdD45_q3wV6obUYv4IXHYx";
+const apikey = "YqRScCgClJAqWZWGIJCNrJWkN0Y654nqJ-nMnvxklAPY0DNiNGDHF5c7xWO4lDUMPBPa7BVf9gQjKGKXmbYyNpPoMON5kbcTFPJmH7zpdlvGHdD45_q3wV6obUYv4IXHYx";
 
 const Yelp = {
   searchYelp(term, location, sortBy) {
